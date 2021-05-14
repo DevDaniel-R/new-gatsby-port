ba83f3951e72cd43f80481d41de406ee1c2e959a
Daniel Rodriguez <drodriguez661@gmail.com>
2021-05-14 01:33:22 -0500

    added links and more to project section, also went ahead and worked on the forms portion of my site

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,26 +7,37 @@ const contact = () => {
       <section className="contact-page">
         <article className="contact-form">
           <h3>get in touch</h3>
-          <form>
+          <form method="POST" data-netlify="true" data-netlify-recaptcha="true">
             <div className="formgroup">
               <input
                 type="text"
+                id="name"
                 name="name"
                 placeholder="name"
                 className="form-control"
               />
               <input
                 type="email"
+                id="email"
                 name="email"
                 placeholder="email"
                 className="form-control"
               />
+              <input
+                type="text"
+                id="phone"
+                name="phone"
+                placeholder="phone"
+                className="form-control"
+              />
               <textarea
                 name="message"
+                id="message"
                 rows="5"
                 placeholder="message"
                 className="form-control"
               ></textarea>
+              <div data-netlify-recaptcha="true"></div>
             </div>
             <button type="submit" className="submit-btn btn">
               submit here
